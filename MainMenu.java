4f1551ed2c786d72adf81585899906ebdd883edd
nchan8150 <nchan1@stuy.edu>
2018-01-06 13:07:24 -0500

    ActionListener added to MainMenu

--- MainMenu.java
+++ MainMenu.java
@@ -57,5 +57,36 @@ public class MainMenu {
 				window.getContentPane().getHeight()/2);
 		bkgImageContainer.setLocation(0, 0); 
 		bkgImageContainer.setVisible(true);
-		
+
+		errorMessage = new JLabel("Error: grid is too small to fit the selected ships");
+		errorMessage.setForeground(Color.RED);
+		errorMessage.setFont(new Font("Impact", Font.PLAIN, 24));
+		errorMessage.setSize(440, 40);
+		errorMessage.setLocation(window.getWidth()/2 - errorMessage.getWidth()/2,
+				window.getHeight()-errorMessage.getHeight() - 30);
+		errorMessage.setVisible(false);
+
+		startGame = new JButton("Start Game");
+		startGame.setSize(200, 100);
+		startGame.setLocation(150, bkgImageContainer.getHeight() + 50);
+		startGame.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				window.getContentPane().remove(startGame);
+				window.getContentPane().remove(bkgImageContainer);
+				window.getContentPane().remove(gridSizeBtn);
+				window.getContentPane().remove(battleshipSize);
+				window.getContentPane().remove(cruiserSize);
+				window.getContentPane().remove(destroyerSize);
+				window.getContentPane().remove(submarineSize);
+				window.getContentPane().remove(battleshipCount);
+				window.getContentPane().remove(cruiserCount);
+				window.getContentPane().remove(destroyerCount);
+				window.getContentPane().remove(submarineCount);
+				window.getContentPane().revalidate();
+				window.getContentPane().repaint();
+				window.getContentPane().setBackground(Color.WHITE);
+				isImageVisible = false;
+			}	
+		});
 	
