ec1991d602dc439d780d62e62e90e374b89b2a20
nchan8150 <nchan1@stuy.edu>
2018-01-06 08:15:59 -0500

    added GUI stuff(buttons and labels) for MainMenu

--- MainMenu.java
+++ MainMenu.java
@@ -6,3 +6,14 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
+public class MainMenu {
+	
+    private JFrame window;
+    private ImageIcon backgroundImageIcon;
+    private JLabel bkgImageContainer;
+    private JButton gridSizeBtn;
+    private JButton startGame;
+    private JButton battleshipSize, cruiserSize, destroyerSize, submarineSize;
+    private JButton battleshipCount, cruiserCount, destroyerCount, submarineCount;
+    private JLabel errorMessage;
