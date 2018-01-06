7d074712e1721f6f0724079cc12691984e53413f
nchan8150 <nchan1@stuy.edu>
2018-01-05 21:08:11 -0500

    Imported everything we need for MainMenu

--- MainMenu.java
+++ MainMenu.java
@@ -1,1 +1,8 @@
-//MainMenu
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
