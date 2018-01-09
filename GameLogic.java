89ada5ea31153a0f7af7cdfe1838c47572393be4
Il Kyu Lee <IKLee@Ils-MacBook-Air.fios-router.home>
2018-01-09 17:26:07 -0500

    Added comments and organized variables into three groups: grid-related, shipsize-related, and count-related

--- GameLogic.java
+++ GameLogic.java
@@ -8,16 +8,20 @@ import javax.swing.JPanel;
 
 public class GameLogic{
 
+    //default grid size
     public static int boardSize = 10;
+
+    //default size 
     public static int battleshipSize = 4;
     public static int cruiserSize = 3;
     public static int destroyerSize = 2;
     public static int submarineSize = 1;
+
+    //default count
     public static int battleshipCount = 1;
     public static int cruiserCount = 2;
     public static int destroyerCount = 3;
     public static int submarineCount = 4;
-
 
 
 
