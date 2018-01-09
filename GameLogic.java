632ad6c62b9d577c8b6775ab398ccd5f45b4cc51
Il Kyu Lee <IKLee@Ils-MacBook-Air.fios-router.home>
2018-01-09 17:18:28 -0500

    Declared instance variables for grid size, default ship sizes, and default ship counts

--- GameLogic.java
+++ GameLogic.java
@@ -5,3 +5,20 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+public class GameLogic{
+
+    public static int boardSize = 10;
+    public static int battleshipSize = 4;
+    public static int cruiserSize = 3;
+    public static int destroyerSize = 2;
+    public static int submarineSize = 1;
+    public static int battleshipCount = 1;
+    public static int cruiserCount = 2;
+    public static int destroyerCount = 3;
+    public static int submarineCount = 4;
+
+
+
+
+}
