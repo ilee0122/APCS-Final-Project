6bcbacb167595b6ab7b2ecbb7f039221b0d2c202
nchan8150 <nchan1@stuy.edu>
2018-01-11 18:00:59 -0500

    added constructor

--- ShipPiece.java
+++ ShipPiece.java
@@ -5,3 +5,18 @@ public class ShipPiece {
 	private Image shipPieceAlive;
 	private boolean shipIsDead;
 	boolean isPlayer1;
+
+    /*
+	 * Constructor that has a boolean to determine which player the ship piece
+	 * belongs to. false is player 2, true is player 1
+	 */
+	public ShipPiece(boolean isPlayer1) {
+		this.isPlayer1 = isPlayer1;
+		// sets the image based on which player it belongs too
+		if (isPlayer1)
+			shipPieceAlive = new ImageIcon("Player1.png").getImage();
+		else
+			shipPieceAlive = new ImageIcon("Player2.png").getImage();
+		shipIsDead = false;
+	}
+
