a543716d65fdbe52cc1be44ebee0e893911eb88d
Il Kyu Lee <IKLee@Ils-MacBook-Air.fios-router.home>
2018-01-10 21:30:06 -0500

    Created and filled out the BetweenTurnsScreen constructor

--- BetweenTurnsScreen.java
+++ BetweenTurnsScreen.java
@@ -16,5 +16,19 @@ public class BetweenTurnsScreen implements MouseListener{
     private Grid grid;
     private SmallGrid small;
 
-
+    public BetweenTurnsScreen(JPanel theWindow, Grid grid, SmallGrid small){
+	window = theWindow;
+	backgroundImageIcon = new ImageIcon("NextTurn.png");
+	Image bkgImage = backgroundImageIcon.getImage();
+	Image scaledBkgImage = bkgImage.getScaledInstance(window.getWidth(),
+							  window.getHeight(), BufferedImage.SCALE_FAST);
+	ImageIcon scaledBkgImageIcon = new ImageIcon(scaledBkgImage);
+	bkgImageContainer = new JLabel(scaledBkgImageIcon);
+	bkgImageContainer.setSize(window.getWidth(), 
+				  window.getHeight());
+	bkgImageContainer.setLocation(0, 0); 
+	isImageVisible = true;
+	this.grid = grid;
+	this.small = small;
+    }
 }
