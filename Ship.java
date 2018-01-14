792541ebda13566c05d260c453a14a22be49f69d
nchan8150 <nchan1@stuy.edu>
2018-01-13 19:15:53 -0500

    instance variables and constructors

--- Ship.java
+++ Ship.java
@@ -1,3 +1,20 @@
 import java.awt.Point;
 import java.util.ArrayList;
 
+public class Ship {
+
+	private ShipPiece[] pieces;
+	private Point startingPosition;
+
+	/*
+	 * Constructor. Sets pieces equal to the list parameter
+	 */
+	Ship(ShipPiece[] list) {
+		pieces = list;
+		startingPosition = new Point(0,0);
+	}
+	
+	Ship(ArrayList<ShipPiece> list){
+		pieces = list.toArray(new ShipPiece[0]);
+		startingPosition = new Point(0,0);
+	}
