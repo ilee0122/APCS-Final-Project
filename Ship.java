7ef69edae7b0af50d4bcf042bc8929b6895bd50e
nchan8150 <nchan1@stuy.edu>
2018-01-13 20:13:51 -0500

    added the rest of Ship.java

--- Ship.java
+++ Ship.java
@@ -18,3 +18,32 @@ public class Ship {
 		pieces = list.toArray(new ShipPiece[0]);
 		startingPosition = new Point(0,0);
 	}
+
+    /*
+	 * If all the ship pieces are dead, it returns that the ship is dead
+	 */
+	public boolean checkIfDead() {
+		boolean isDead = true;
+		for (int i = 0; i < pieces.length; i++) {
+			if (!pieces[i].isDestroy()) {
+				isDead = false;
+			}
+		}
+		return isDead;
+	}
+
+	/*
+	 * returns the array of ship pieces
+	 */
+	public ShipPiece[] getShipPieces() {
+		return pieces;
+	}
+	
+	public void setStartingOffGridPosition(Point sp){
+		startingPosition = sp;
+	}
+	
+	public Point getStartingOffGridPosition(){
+		return startingPosition;
+	}
+}
