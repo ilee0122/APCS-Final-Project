d95bbfd0973b18260bb2d9afc2c6521b7b16e7dc
Il Kyu Lee <IKLee@Ils-MacBook-Air.fios-router.home>
2018-01-14 11:49:49 -0500

    Added javax.swing imports

--- GameOverScreen.java
+++ GameOverScreen.java
@@ -3,3 +3,9 @@ import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
