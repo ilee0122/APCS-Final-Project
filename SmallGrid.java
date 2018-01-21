6270cebd4dd4165bbba752dcea1cc925f0e47556
nchan8150 <nchan1@stuy.edu>
2018-01-21 16:51:19 -0500

    adding comments!

--- SmallGrid.java
+++ SmallGrid.java
@@ -12,25 +12,23 @@ public class SmallGrid extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private Object[][] array;
 	private BufferedImage gridImage;
-	public static final int X_ORIGIN = 23; // X coordinate of the top left
-	public static final int Y_ORIGIN = 39; // y coordinate of the top left
-	public static final int TILE_SIZE = 20; // size of the tile spaces
-	public static final int BORDER_SIZE = 3; // size of the border between tiles
-	public static final int PIECE_SIZE = 18; // size of the pieces that appear
+	public static final int X_ORIGIN = 23; //x coordinate of the top left
+	public static final int Y_ORIGIN = 39; //y coordinate of the top left
+	public static final int TILE_SIZE = 20; //size of the tile spaces
+	public static final int BORDER_SIZE = 3; //size of the border between tiles
+	public static final int PIECE_SIZE = 18; //size of the pieces that appear
 
-	/*
-	 * Constructor that takes a 2d array object
-	 */
+    // Constructor that takes a 2d array object
 	public SmallGrid(Object[][] array) {
 		this.array = array;
 		
-		// sets the background to white
+		//Sets the background to white
 		setBackground(Color.WHITE);
 		setPreferredSize(new Dimension((X_ORIGIN + 2 + (TILE_SIZE+BORDER_SIZE)*array.length), 
 				Y_ORIGIN+ 2 + ((TILE_SIZE+BORDER_SIZE)*array.length)));
 		setSize(getPreferredSize());
 		
-		// loads the grid file
+		//Loads the grid file
 		try {
 			gridImage = ImageIO.read(new File("gridSmallLabels.png"));
 		} catch (IOException e) {
@@ -42,16 +40,15 @@ public class SmallGrid extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
-		// draws the grid image
+		//Draws the grid image
 		g2.drawImage(gridImage, 0, 15, this);
 
-		// loops through the array
+		//Loops through the array
 		for (int i = 0; i < array.length; i++) {
 			for (int j = 0; j < array[i].length; j++) {
-				// if there is a ship piece at the location
+				//If there is a ship piece at the location...
 				if ((array[i][j]).getClass().getName().equals("ShipPiece")) {
-					// draw the image of the ship piece at the proper grid
-					// location
+					//Draw the image of the ship piece at the proper grid location
 					g2.drawImage(((ShipPiece) array[i][j]).getShipImage(), 
 							X_ORIGIN + 2 + ((TILE_SIZE + BORDER_SIZE) * i) + BORDER_SIZE/2,
 							Y_ORIGIN + 2 + ((TILE_SIZE + BORDER_SIZE) * j) + BORDER_SIZE/2,
@@ -61,16 +58,12 @@ public class SmallGrid extends JPanel {
 		}
 	}
 
-	/*
-	 * Returns the array
-	 */
+    //Returns the array
 	public Object[][] getArray() {
 		return array;
 	}
 
-	/*
-	 * Sets the array
-	 */
+    //Sets the array
 	public void setArray(Object[][] array) {
 		this.array = array;
 	}
